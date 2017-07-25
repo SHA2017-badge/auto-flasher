af8eac6fcaf7122912d070f3d10eb15bcc4f08c1
Bas van Sisseren <bas@vansisseren.nl>
2017-07-25 18:58:59 +0200

    add type 'f'

--- updateZip.sh
+++ updateZip.sh
@@ -4,13 +4,13 @@ set -e
 
 cd firmware/locfd
 
-for type in sl n; do
-	cp media/hacking-$type.png media/hacking.png
+for type in sl n f; do
+	cp media/badge_type_$type.png media/badge_type.png
 
 	# create with maximum compression to a temporary file.
-	zip -r -9 ../locfd-$type.zip.$$ .
+	zip -r -9 ../locfd-$type.zip.$$ . -x .ds_store
 
-	rm media/hacking.png
+	rm media/badge_type.png
 
 	# always do an atomic move.
 	mv ../locfd-$type.zip.$$ ../locfd-$type.zip
